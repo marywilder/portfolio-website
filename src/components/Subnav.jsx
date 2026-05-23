ccb8ae90476306dea74a7406527e48b540c910e8
marywilder <103593057+marywilder@users.noreply.github.com>
2026-05-23 17:07:20 +0000

    changes to subnav

--- src/components/Subnav.jsx
+++ src/components/Subnav.jsx
@@ -5,17 +5,17 @@ export default function Subnav() {
   return (
              <div className="sub-nav">
                     <div className="product-link-wrapper">
-                      <NavLink to='/productdesign' className="product-link">product design</NavLink>
+                      <NavLink to='/productdesign' className="product-link" onClick={() => window.scrollTo(0, 0)}>product design</NavLink>
                       <div className="wavy-line"></div>
                     </div>
                     <span className="subnav-divider">✿</span>
                     <div className="web-dev-link-wrapper">
-                      <NavLink to='/webdev' className="web-dev-link">web dev</NavLink>
+                      <NavLink to='/webdev' className="web-dev-link" onClick={() => window.scrollTo(0, 0)}>web dev</NavLink>
                       <div className="wavy-line"></div>
                     </div>
                     <span className="subnav-divider">✿</span>
                     <div className="illustration-link-wrapper">
-                      <NavLink to='/illustration' className="illustration-link">graphic design</NavLink>
+                      <NavLink to='/illustration' className="illustration-link" onClick={() => window.scrollTo(0, 0)}>graphic design</NavLink>
                       <div className="wavy-line"></div>
                     </div>
               </div>
